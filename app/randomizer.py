facf9f29fee58664750ae0bcbacc877002f47d6c
Arne Vangheel <94957070+ArneVangheel@users.noreply.github.com>
2022-11-15 14:12:19 +0100

    Update randomizer.py

--- app/randomizer.py
+++ app/randomizer.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+import json
+
 app = FastAPI()
 orders = {}
 class Order(BaseModel):
@@ -14,8 +16,8 @@ class Order(BaseModel):
 async def create_order(item: Order):
     orderid = len(orders)
     orders[orderid] = item.dict()
-    return orders[orderId]
+    return orders[orderid]
 
 @app.get("/orders")
-async def get_orders():
-    return {"orders": orders}
+async def get_orders(id: int):
+    return orders
