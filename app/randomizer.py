baa8795689c0976234718f6b5e28d4c843926cd3
Arne Vangheel <94957070+ArneVangheel@users.noreply.github.com>
2022-11-15 13:12:19 +0100

    Update randomizer.py

--- app/randomizer.py
+++ app/randomizer.py
@@ -1,22 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 app = FastAPI()
-origins = [
-    "http://localhost/",
-    "http://localhost:8080/",
-    "https://localhost.tiangolo.com/",
-    "http://127.0.0.1:5500/"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=[""],
-    allow_headers=[""],
-)
-
 orders = {}
 class Order(BaseModel):
     first_name: str
@@ -29,7 +13,7 @@ class Order(BaseModel):
 @app.post("/order")
 async def create_order(item: Order):
     orderid = len(orders)
-    orders[{"OrderId": orderid}] = item.dict()
+    orders["OrderId": orderid] = item.dict()
     return orders[orderId]
 
 @app.get("/orders")
